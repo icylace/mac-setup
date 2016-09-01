9689d897a9066b370a5c70f05f93321c4f682982
Ron Martinez <ramartin.net@gmail.com>
2016-08-31 23:32:30 -0400

    WIP

--- homebrew/specific/python.sh
+++ homebrew/specific/python.sh
@@ -16,24 +16,3 @@ brew install pyenv
 # https://www.python.org/
 pyenv install 2.7.12
 pyenv global 2.7.12
-
-# eg
-# Useful examples at the command line.
-# https://github.com/srsudar/eg
-pip install --upgrade eg
-
-# Ohmu
-# View disk space usage in your terminal.
-# https://github.com/paul-nechifor/ohmu
-pip install --upgrade ohmu
-
-# # pip-tools
-# # A set of command line tools to help you keep your pip-based packages fresh,
-# # even when you've pinned them.
-# # https://github.com/nvie/pip-tools
-# pip install --upgrade pip-tools
-
-# searchcmd
-# Search the internet for command line commands from the command line.
-# https://github.com/jimmyppi/searchcmd
-pip install --upgrade searchcmd
