3480f35e02b294bfdf2978860eb02c769595dcaa
Ron Martinez <ramartin.net@gmail.com>
2021-05-03 13:35:34 -0400

    wip

--- npm/npm.sh
+++ npm/npm.sh
@@ -6,10 +6,11 @@ blue '
 + - - - - - - - - - - - - - - - - - - - -
 '
 
-# # deps-ok
-# # Fast checking of top level dependencies based on version numbers.
-# # https://www.npmjs.com/package/deps-ok
-# npm install --global deps-ok
+# ESLint
+# Find and fix problems in your JavaScript code.
+# https://eslint.org/
+# https://github.com/eslint/eslint
+npm install --global eslint
 
 # np
 # A better `npm publish`
@@ -17,12 +18,12 @@ blue '
 # https://www.npmjs.com/package/np
 # https://github.com/sindresorhus/np#config
 npm install --global np
-# TODO:
-# Do this on the project level.
+# To sign Git tags, do this on the project level.
 # # https://github.com/sindresorhus/np#signed-git-tag
 # npm config set sign-git-tag true
 
-# # npm-check-updates
-# # Finds newer versions of dependencies than what your package.json allows.
-# # https://www.npmjs.com/package/npm-check-updates
-# npm install --global npm-check-updates
+# npm-check-updates
+# Finds newer versions of dependencies than what your package.json allows.
+# https://github.com/raineorshine/npm-check-updates
+# https://www.npmjs.com/package/npm-check-updates
+npm install --global npm-check-updates
